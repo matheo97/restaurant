630cb10c503adcc005dc8ed73cf02353c60e4067
Mateo Salazar <msalazar@velostics.com>
2022-03-07 17:58:54 -0500

    Add morgan logger

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as logger from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.use(logger('[:date[iso]] :method :url :status :response-time ms'));
 
   await app.listen(Number(process.env.PORT));
 }
