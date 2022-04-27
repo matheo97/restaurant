e644ad0f1e3ec607fb58d8192a4dbfeff0e259f7
Juan Sebastian Fernandez <jfernandezdev14@gmail.com>
2022-04-26 21:49:34 -0500

    Adding transaction module to app

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,3 +3,4 @@ export { UserModule } from './user/user.module';
 export { AuthModule } from './auth/auth.module';
 export { ItemModule } from './item/item.module';
 export { OrderModule } from './order/order.module';
+export { TransactionModule } from './transaction/transaction.module';
