517fe96f705da206070f08eff2212e08e917ab7f
fernandogcabal <fernandogcabal@mac-fernandogcabal.local>
2022-05-14 23:16:09 -0500

    Adding expeneses changes

--- src/modules/expense/expense.controller.ts
+++ src/modules/expense/expense.controller.ts
@@ -24,6 +24,9 @@ import { User } from 'entities/User.entity';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ExpenseService } from './expense.service';
 import { Request } from 'express';
+import { ParseOrderByPipeClients } from './expense.pipes';
+import { PageResponse } from 'src/constants/PageResponse';
+import { DeleteResult } from 'typeorm';
 
 @Controller('/expense')
 @ApiTags('Expenses')
@@ -42,6 +45,60 @@ export class ExpenseController {
     @Req() {user }: Request
   ): Promise<Expense>{
     return this.expenseService.getExpenseInfoById(expenseId, (user as User).companyId);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all info of all Expenses based on its Id'})
+  @ApiOkResponse({ description: 'All info of all Expenses', type: Expense})
+  async find(
+    @Req() { user }: Request,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+    @Query('q') searchCriteria: string,
+    @Query('order', ParseOrderByPipeClients) order: 'ASC' | 'DESC',
+    @Query('orderBy', ParseOrderByPipeClients) orderBy: string
+  ): Promise<PageResponse<Expense>>{
+    return this.expenseService.findExpense(
+      (user as any).company.id,
+      page,
+      pageSize,
+      searchCriteria,
+      order,
+      orderBy
+    );
+  }
+
+  @Put('/:id')
+  @ApiOperation({summary: 'Update expense'})
+  @ApiOkResponse({description: 'Expense correctly updated', type: Expense})
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() expense: Expense
+  ): Promise<Expense>{
+    return this.expenseService.update(id, expense, (expense as any).companyId);
+  }
+
+  @Delete('/:id')
+  @ApiOperation({ summary: 'Delete expense based on Expense ID'})
+  @ApiOkResponse({
+    description: 'Delete expense based on Expense ID',
+    type: DeleteResult,
+  })
+  async delete(
+    @Req() {user}: Request,
+    @Param('id', ParseUUIDPipe) id: string
+  ): Promise<DeleteResult> {
+    return this.expenseService.deleteExpense(id, (user as any).company.id);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Create expense'})
+  @ApiOkResponse({ description: 'Expense correctly created', type: Expense})
+  async created(
+    @Req() {user}: Request,
+    @Body() expenseBody: Expense
+  ): Promise<Expense>{
+    return this.expenseService.createExpense(expenseBody, (user as any).companyId);
   }
 
 }
--- src/modules/expense/expense.service.ts
+++ src/modules/expense/expense.service.ts
@@ -1,12 +1,52 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { Expense } from "entities/Expense.entity";
+import { PageResponse } from "src/constants/PageResponse";
+import { DeleteResult } from "typeorm";
 import { ExpenseDAO } from "./expense.dao";
 
 @Injectable()
 export class ExpenseService{
-    constructor(private readonly expenseDao: ExpenseDAO){}
+    constructor(
+        @InjectRepository(Expense)
+        private readonly expenseDao: ExpenseDAO){}
 
+    async createExpense(expense: Expense, companyId: string): Promise<Expense>{
+        return this.expenseDao.save(expense);
+    }
+
+    async update(id: string, expense: Expense, companyId: string): Promise<Expense> {
+        const existingExpense = await this.getExpenseInfoById(id, companyId);
+        if (!existingExpense) {
+            throw new BadRequestException('Expense does not exist');
+        }
+        return this.expenseDao.save({ ...expense, id});   
+    }
+    
     async getExpenseInfoById(id: string, companyId?: string): Promise<Expense>{
         return this.expenseDao.getExpenseInfoById(id,companyId);
     }
+
+    async deleteExpense(id: string, companyId?: string): Promise<DeleteResult>{
+        return this.expenseDao.delete(id, companyId);
+    }
+
+    async findExpense(
+        companyId: string,
+        page: number,
+        pageSize: number,
+        searchCriteria: string,
+        order: 'ASC' | 'DESC',
+        orderBy: string
+    ): Promise<PageResponse<Expense>> {
+        return this.expenseDao.find(
+            companyId,
+            page,
+            pageSize,
+            searchCriteria,
+            order,
+            orderBy
+        );
+    }
+    
 }
