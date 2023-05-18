2e4f8731c05ea2e1febe3da76c3ce2b6e0280ede
jfernandezdev14 <99499519+jfernandezdev14@users.noreply.github.com>
2023-05-18 08:29:23 -0500

    1/feature/heroku conf (#10)
    * Adding Heroku configuration to test
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update main.ts
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update main.ts

--- ormconfig.js
+++ ormconfig.js
@@ -3,24 +3,41 @@ require('dotenv').config();
 
 const env = process.env.NODE_ENV;
 
-module.exports = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database:
-    env === 'test' ? process.env.DB_TEST_DATABASE : process.env.DB_DATABASE,
-  synchronize: false,
-  migrationsRun: true,
-  migrations: ['db/migrations/*.ts'],
-  entities: ['entities/*.ts'],
-  ...(process.env.DB_SSL && {
+if (process.env.REMOTE_SERVER === 'HEROKU') {
+  module.exports = {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    migrationsRun: true,
+    migrations: ['db/migrations/*.ts'],
+    entities: ['entities/*.ts'],
     ssl: {
       rejectUnauthorized: false,
     },
-  }),
-  cli: {
-    migrationsDir: 'db/migrations',
-  },
-};
+    cli: {
+      migrationsDir: 'db/migrations',
+    },
+  };
+} else {
+  module.exports = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:
+      env === 'test' ? process.env.DB_TEST_DATABASE : process.env.DB_DATABASE,
+    synchronize: false,
+    migrationsRun: true,
+    migrations: ['db/migrations/*.ts'],
+    entities: ['entities/*.ts'],
+    ...(process.env.DB_SSL && {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    cli: {
+      migrationsDir: 'db/migrations',
+    },
+  };
+}
